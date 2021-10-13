357904c671635a98043f9b5621929650bd0cc3ca
Rob Pomeroy <rob@pomeroy.me>
2021-10-13 10:42:53 +0100

    Improve cross-platform handling of fzf configuration

---
+++ roles/packages/files/Debian.fzf.bash
@@ -0,0 +1,7 @@
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/share/doc/fzf/examples/key-bindings.bash"
